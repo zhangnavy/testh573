fb2191fdacf3dc04d94d08ae5e0963f224597bda
chenchao111 <495075945@qq.com>
2017-03-17 10:39:07 +0800

    主键和外健

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -3,16 +3,24 @@ var router = express.Router();
 
 var articleModel = require('../mongodb/db').articleModel;
 
+//引入markdown模块
+var markdown = require('markdown').markdown;
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     //读取数据库中所有文章列表的信息
 
     articleModel.find({})
         .populate('user')
-        .exec(function (err, doc) {
+        .exec(function (err, articles) {
             if (!err) {
                 req.flash('success', '获取文章列表信息成功');
-                res.render('index', {title: '首页标题', articles: doc});
+
+                articles.forEach(function (article, index) {
+                    article.content = markdown.toHTML(article.content);//让所有文章的内容支持markdown
+                });
+
+                res.render('index', {title: '首页标题', articles: articles});
             } else {
                 req.flash('error', '获取文章列表信息失败');
                 res.redirect('back');
